7a82ac828579e5012559e170b3e57e3e2674e87e
Alex Schenck <linuxbeak@gmail.com>
2022-03-04 15:37:07 -0500

    This is a good/fixed upload

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ resource "aws_instance" "web" {
   ami           = "ami-830c94e3"
   instance_type = "t2.micro"
   tags = {
-    Name =    "HelloWorld"
+    Name = "HelloWorld"
   }
 
   vpc_security_group_ids = [aws_security_group.web-sg.id]
